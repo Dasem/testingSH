a4429e12f3234e1562296b3f87d65cc2d8287c17
Dasem <gfhflbuvf46@mail.ru>
2019-06-22 22:32:30 +0500

    + maven

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
-﻿@echo off
-mvn clean install
+@echo off
+call "apache-maven-3.6.1/bin/mvn" clean install
 del /q deployed
 md deployed
 echo "Move start.bat..."
@@ -10,5 +10,5 @@ echo "Move readme.txt..."
 copy readme.txt deployed\readme.txt
 echo "Move testing-1.0.4.jar..."
 copy target\testing-1.0.4.jar deployed\testing-1.0.4.jar
-echo "Build success, look adt folder 'deployed' deployed"
+echo "Build success, look at folder 'deployed' deployed"
 pause
